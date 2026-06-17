77399f4fe6fb08eec1d08f4c751dc4588b78e114
Kimthina Ly <kimthinaly7@gmail.com>
2026-06-17 15:19:43 +0700

    feat: write project structure

---
+++ src/pages/MessageUI.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ChatUIHeader from '@/components/ChatUIHeader.vue';
+import MessageArea from '@/components/MessageArea.vue';
+import InputBar from '@/components/InputBar.vue';
+
+</script>
+<template>
+    <div style="display: flex; flex-direction: column;">
+        <ChatUIHeader />
+        <MessageArea />
+        <InputBar />
+    </div>
+</template>
