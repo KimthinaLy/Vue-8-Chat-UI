2a4c97a13aba45ac93360d5023b603ad746bb028
Kimthina Ly <kimthinaly7@gmail.com>
2026-06-17 17:05:56 +0700

    feat: implement search input

--- src/components/InputBar.vue
+++ src/components/InputBar.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { useChatStore } from '@/stores/chatStore';
+import { ref } from 'vue'
+
+const chatStore = useChatStore()
+const content = ref('')
+
+function sendUserMessage() {
+    if (!content.value.trim()) return
+    chatStore.sendMessage(content.value)
+    content.value = ''
+}
 </script>
 <template>
-    <p>Input bar will diplay here</p>
+    <input v-model="content" type="text" placeholder="message" @keyup.enter="sendUserMessage" />
+    <button @click="sendUserMessage">send</button>
 </template>
