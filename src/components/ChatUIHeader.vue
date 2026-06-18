7d0ceddf1d396322b4dcecc19e65136898713e69
Kimthina Ly <kimthinaly7@gmail.com>
2026-06-18 10:12:18 +0700

    feat: implement autoscroll to the bottom and isTyping(in progress)

--- src/components/ChatUIHeader.vue
+++ src/components/ChatUIHeader.vue
@@ -1,5 +1,36 @@
 <script setup lang="ts">
+import { useChatStore } from '@/stores/chatStore';
+
+const chatStore = useChatStore()
 </script>
 <template>
-    <h2>Header will display here</h2>
+    <h2 style="margin-bottom: 0;">Header will display here</h2>
+    <Transition name="fade">
+        <p style="font-size: 16px; color: gray; margin: 0;">{{ chatStore.isTyping ? "Typing..." : "Online" }}</p>
+    </Transition>
 </template>
+<style>
+.fade-enter-from {
+    opacity: 0;
+}
+
+.fade-enter-active {
+    transition: opacity 1s ease;
+}
+
+.fade-enter-to {
+    opacity: 1;
+}
+
+.fade-leave-from {
+    opacity: 0;
+}
+
+.fade-leave-active {
+    transition: opacity 1s ease;
+}
+
+.fade-leave-to {
+    opacity: 1;
+}
+</style>
--- src/components/MessageArea.vue
+++ src/components/MessageArea.vue
@@ -1,13 +1,38 @@
 <script setup lang="ts">
 import { useChatStore } from '@/stores/chatStore';
 import MessageBubble from './MessageBubble.vue';
+import { ref, watch, nextTick } from 'vue'
 
 const chatStore = useChatStore()
+const container = ref<HTMLElement | null>(null)
 
 
+
+watch(chatStore.messageList, async () => {
+
+    await nextTick()  //wait until vue finish updating dom
+    if (container.value) {
+        container.value.scrollTop = container.value?.scrollHeight
+    }
+}, { deep: true, immediate: true })
 </script>
 <template>
-    <div style="padding: 1rem; display: flex; flex-direction: column;">
-        <MessageBubble v-for="msg in chatStore.messageList" :key="msg.id" :message="msg" />
+    <div ref="container" style="padding: 1rem; height: 80vh; overflow-y: auto; display: flex; flex-direction: column;">
+        <TransitionGroup tag="div" style="padding: 1rem; display: flex; flex-direction: column;">
+            <MessageBubble v-for="msg in chatStore.messageList" :key="msg.id" :message="msg" />
+        </TransitionGroup>
     </div>
 </template>
+<style scoped>
+.v-enter-from {
+    opacity: 0;
+}
+
+.v-enter-active {
+    transition: opacity 1s ease;
+}
+
+.v-enter-to {
+    opacity: 1;
+}
+</style>
