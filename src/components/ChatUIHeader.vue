22c04f8c81f584ed694e4ef42f2992ccb06732e9
Kimthina Ly <kimthinaly7@gmail.com>
2026-06-18 10:19:44 +0700

    feat: implement isTyping../online header

--- src/components/ChatUIHeader.vue
+++ src/components/ChatUIHeader.vue
@@ -5,32 +5,24 @@ const chatStore = useChatStore()
 </script>
 <template>
     <h2 style="margin-bottom: 0;">Header will display here</h2>
-    <Transition name="fade">
-        <p style="font-size: 16px; color: gray; margin: 0;">{{ chatStore.isTyping ? "Typing..." : "Online" }}</p>
+    <Transition name="fade" mode="out-in">
+        <p v-if="chatStore.isTyping" key="typing" style="font-size: 16px; color: gray; margin: 0;">Typing...</p>
+        <p v-else key="online" style="font-size: 16px; color: gray; margin: 0;">Online</p>
     </Transition>
 </template>
 <style>
-.fade-enter-from {
-    opacity: 0;
-}
-
-.fade-enter-active {
-    transition: opacity 1s ease;
-}
-
-.fade-enter-to {
-    opacity: 1;
-}
-
-.fade-leave-from {
-    opacity: 0;
-}
-
-.fade-leave-active {
-    transition: opacity 1s ease;
-}
-
+.fade-enter-from,
 .fade-leave-to {
+    opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-to,
+.fade-leave-from {
     opacity: 1;
 }
 </style>
